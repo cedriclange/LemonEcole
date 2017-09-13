3336a07486b9de788475ed44b8552059b06dc647
lange <langemataso@gmail.com>
2017-09-13 13:18:34 +0200

    configure razor page

--- SchoolSolution.Web/Controllers/HomeController.cs
+++ SchoolSolution.Web/Controllers/HomeController.cs
@@ -32,20 +32,6 @@ namespace SchoolSolution.Web.Controllers
             return View(model);
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View();
